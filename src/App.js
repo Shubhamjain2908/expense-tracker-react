0b9551b12c739ccdf9850a08cf9e03e2d0772329
Shubham Jain <shubhamjain2908@gmail.com>
2020-04-18 01:42:15 +0530

    balance added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Balance } from './components/Balance';
 import { Header } from './components/Header';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
